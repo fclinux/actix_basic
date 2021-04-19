494d531f5ea50c3c741038b71f78926cbe7aeb7a
fclinux <fclinux@DESKTOP-ETOSOJ0.localdomain>
2021-04-20 05:52:42 +0700

    3th  update

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,6 @@
 use actix_files as fs;
-use actix_utils::mpsc;
+use std::sync::mpsc;
+// use actix_utils::mpsc;
 use actix_session::{ Session};
 use actix_web::http::{header, Method, StatusCode};
 use actix_web::{
@@ -13,7 +14,7 @@ use actix_web::{
 
 // Simple index handler
 #[get("/welcome")]
-async pub fn welcome(session: Session, req: HttpRequest) -> Result<HttpResponse>{
+pub async fn welcome(session: Session, req: HttpRequest) -> Result<HttpResponse, Error>{
     println!("{:?}", req);
 
     // Session
@@ -29,22 +30,22 @@ async pub fn welcome(session: Session, req: HttpRequest) -> Result<HttpResponse>
     // response
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
-        .body(include_str!("../static/welcome.html"))
+        .body(include_str!("static/welcome.html"))
     )
 }
 
 #[get("/favicon")]
-async pub fn favicon() -> Result<fs::NamedFile>{
+pub async fn favicon() -> Result<fs::NamedFile,Error >{
     Ok(fs::NamedFile::open("static/favicon.ico")?)
 }
 
 //404 handlers
-async fn p404() -> Result<fs::NamedFile>{
+pub async fn p404() -> Result<fs::NamedFile, Error>{
     Ok(fs::NamedFile::open("static/404.html")?.set_status_code(StatusCode::NOT_FOUND))
 }
 
 /// response body
-async pub fn response_body(path: web::Path<String>) -> HttpResponse {
+pub async fn response_body(path: web::Path<String>) -> HttpResponse {
     let text = format!("Hello {}!", *path);
 
     let (tx, rx_body) = mpsc::channel();
@@ -54,7 +55,7 @@ async pub fn response_body(path: web::Path<String>) -> HttpResponse {
 }
 
 /// handler with path parameters like `/user/{name}/`
-async pub fn with_param(
+pub async fn with_param(
     req: HttpRequest,
     web::Path((name,)): web::Path<(String,)>,
 ) -> HttpResponse {
